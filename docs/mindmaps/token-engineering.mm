b6a8a263f1467af6d3593f559c91dd70dc6e1935
Peter Alexander <peter.alexander@prodatalab.com>
2018-06-28 15:05:24 -0600

    added more to token-engineering.mm

--- docs/mindmaps/token-engineering.mm
+++ docs/mindmaps/token-engineering.mm
@@ -3,7 +3,7 @@
 <node TEXT="Token Engineering" FOLDED="false" ID="ID_1047944362" CREATED="1530209264328" MODIFIED="1530209272987" STYLE="oval">
 <font SIZE="18"/>
 <hook NAME="MapStyle">
-    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
@@ -63,8 +63,8 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="Glossary" FOLDED="true" POSITION="right" ID="ID_1537660174" CREATED="1530209344523" MODIFIED="1530209350163">
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Glossary" POSITION="right" ID="ID_1537660174" CREATED="1530209344523" MODIFIED="1530209350163">
 <edge COLOR="#ff0000"/>
 <node TEXT="Token Engineering" ID="ID_633833220" CREATED="1530209351935" MODIFIED="1530209357200"/>
 <node TEXT="Crypto-Asset Valuation" ID="ID_140430192" CREATED="1530209358344" MODIFIED="1530209366845"/>
@@ -93,7 +93,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node ID="ID_1920611877" CREATED="1530211481715" MODIFIED="1530211481715" LINK="https://goo.gl/dNBJHk"><richcontent TYPE="NODE">
@@ -108,7 +107,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="Note: TE Series Part II" ID="ID_298713392" CREATED="1530211481716" MODIFIED="1530211967176"/>
@@ -134,8 +132,54 @@
 <node TEXT="Link: https://goo.gl/YdrP5K" ID="ID_1398676005" CREATED="1530212594525" MODIFIED="1530212629451" LINK="https://goo.gl/YdrP5K"/>
 <node TEXT="About: Design Methodology" ID="ID_1031907052" CREATED="1530212631676" MODIFIED="1530212667175"/>
 </node>
+<node TEXT="Token Curated Registries 1.0" ID="ID_1033841649" CREATED="1530216716188" MODIFIED="1530216769039">
+<node TEXT="Author: Mike Goldin" ID="ID_576494289" CREATED="1530216770657" MODIFIED="1530216781457"/>
+<node TEXT="Link: https://goo.gl/Xt8Uhs" ID="ID_1996402848" CREATED="1530216784154" MODIFIED="1530216804936" LINK="https://goo.gl/Xt8Uhs"/>
 </node>
-<node TEXT="People" FOLDED="true" POSITION="right" ID="ID_904425458" CREATED="1530210206498" MODIFIED="1530210209612">
+<node TEXT="Token Curated Registries 1.1, 2.0 TCRs, new theory and dev updates" ID="ID_698552387" CREATED="1530216849731" MODIFIED="1530216877281">
+<node TEXT="Author: Mike Goldin" ID="ID_474830547" CREATED="1530216881351" MODIFIED="1530216915076"/>
+<node TEXT="Link: https://goo.gl/Xt8Uhs" ID="ID_1233310118" CREATED="1530216915787" MODIFIED="1530216934685" LINK="https://goo.gl/Xt8Uhs"/>
+</node>
+<node TEXT="Objective vs. Subjective Games" ID="ID_613972741" CREATED="1530217036978" MODIFIED="1530217052259">
+<node TEXT="Author: Mike Goldin" ID="ID_1890123095" CREATED="1530217055163" MODIFIED="1530217064955"/>
+<node TEXT="Link: https://goo.gl/Xt8Uhs" ID="ID_763881366" CREATED="1530217066293" MODIFIED="1530217085551" LINK="https://goo.gl/Xt8Uhs"/>
+</node>
+<node TEXT="Incentive Alignment in Token Curated Registries" ID="ID_893586086" CREATED="1530217260651" MODIFIED="1530217275729">
+<node TEXT="Author: Jelle Gerbrandy" ID="ID_607536177" CREATED="1530217277003" MODIFIED="1530217287932"/>
+<node TEXT="Link: https://goo.gl/zJuspo" ID="ID_1537151380" CREATED="1530217288597" MODIFIED="1530217312325" LINK="https://goo.gl/zJuspo"/>
+</node>
+<node TEXT="Token Curated Playlists #1: Thoughts on staking and consumer applications" ID="ID_1952379173" CREATED="1530217424173" MODIFIED="1530217454803">
+<node TEXT="Author: Felipe Gaucho Pereira" ID="ID_1906794651" CREATED="1530217463092" MODIFIED="1530217479022"/>
+<node TEXT="Link: https://goo.gl/CX9rZH" ID="ID_51256888" CREATED="1530217479632" MODIFIED="1530217500130" LINK="https://goo.gl/CX9rZH"/>
+</node>
+<node TEXT="Token curated playlists #2: throughput &amp; fitness, use cases, UX" ID="ID_1334681548" CREATED="1530217596335" MODIFIED="1530217601838">
+<node TEXT="Author: Felipe Gaucho Pereira" ID="ID_1351779390" CREATED="1530217605055" MODIFIED="1530217620180"/>
+<node TEXT="Link: https://goo.gl/sz85We" ID="ID_1414852948" CREATED="1530217621358" MODIFIED="1530217642241" LINK="https://goo.gl/sz85We"/>
+</node>
+<node TEXT="On the immaturity of tokenized value capture mechanisms" ID="ID_1405283900" CREATED="1530217999921" MODIFIED="1530218002964">
+<node TEXT="" ID="ID_462834008" CREATED="1530218002969" MODIFIED="1530218002969">
+<node TEXT="Author: Felipe Gaucho Pereira" ID="ID_117560816" CREATED="1530218011556" MODIFIED="1530218328713"/>
+</node>
+<node TEXT="Link: https://goo.gl/AXpbFH" ID="ID_109311660" CREATED="1530218013258" MODIFIED="1530218044585" LINK="https://goo.gl/AXpbFH"/>
+</node>
+<node TEXT="Subjective vs. Objective TCRs" ID="ID_1044400265" CREATED="1530218626636" MODIFIED="1530218634027">
+<node TEXT="Author: Moshe Praver" ID="ID_498960107" CREATED="1530218635334" MODIFIED="1530218651272"/>
+<node TEXT="Link: https://goo.gl/1kDNmC" ID="ID_1006852392" CREATED="1530218653858" MODIFIED="1530218679223" LINK="https://goo.gl/1kDNmC"/>
+</node>
+<node TEXT="Incentive Fundamentals for a Token Economy" ID="ID_1693530434" CREATED="1530219212835" MODIFIED="1530219216817">
+<node TEXT="Author: Don Yocham" ID="ID_1892062618" CREATED="1530219220911" MODIFIED="1530219233304"/>
+<node TEXT="Link: https://goo.gl/ZoYLhE" ID="ID_254277728" CREATED="1530219233840" MODIFIED="1530219250902" LINK="https://goo.gl/ZoYLhE"/>
+</node>
+<node TEXT="EasyTCR&#x200a;&#x2014;&#x200a;a UI/UX framework for Token-curated registries" ID="ID_1886118553" CREATED="1530219342883" MODIFIED="1530219348440">
+<node TEXT="Author: DApp DEV" ID="ID_2492364" CREATED="1530219350032" MODIFIED="1530219367900"/>
+<node TEXT="Link: https://goo.gl/jNd11v" ID="ID_1115560949" CREATED="1530219368618" MODIFIED="1530219387013" LINK="https://goo.gl/jNd11v"/>
+</node>
+<node TEXT="The Civil Registry: a TCR for Quality Journalism" ID="ID_1049216540" CREATED="1530219438244" MODIFIED="1530219441281">
+<node TEXT="Author: Nguyet Vuong" ID="ID_418456033" CREATED="1530219442881" MODIFIED="1530219466318"/>
+<node TEXT="LInk: https://goo.gl/WfLHpo" ID="ID_17864208" CREATED="1530219467271" MODIFIED="1530219490633" LINK="https://goo.gl/WfLHpo"/>
+</node>
+</node>
+<node TEXT="People" POSITION="right" ID="ID_904425458" CREATED="1530210206498" MODIFIED="1530210209612">
 <edge COLOR="#00ff00"/>
 <node TEXT="Trent McConaghy" ID="ID_743269120" CREATED="1530210210953" MODIFIED="1530210221432">
 <node TEXT="Blog: Medium" ID="ID_988492645" CREATED="1530211220162" MODIFIED="1530211233058">
@@ -143,6 +187,21 @@
 </node>
 </node>
 <node TEXT="Chris Burniske" ID="ID_1888160032" CREATED="1530210222096" MODIFIED="1530210228545"/>
+<node TEXT="Mike Goldin" ID="ID_196755029" CREATED="1530216627495" MODIFIED="1530216651642">
+<node TEXT="Blog: Medium" ID="ID_292563109" CREATED="1530216681046" MODIFIED="1530216688807">
+<node TEXT="Link: https://goo.gl/Xt8Uhs" ID="ID_265919437" CREATED="1530216689362" MODIFIED="1530216706286" LINK="https://goo.gl/Xt8Uhs"/>
+</node>
+</node>
+<node TEXT="Jelle Gerbrandy" FOLDED="true" ID="ID_556856945" CREATED="1530217110270" MODIFIED="1530217118294">
+<node TEXT="Blog: Medium" ID="ID_1172255919" CREATED="1530217152969" MODIFIED="1530217157901">
+<node TEXT="Link: https://goo.gl/w94xnx" ID="ID_1366708855" CREATED="1530217160496" MODIFIED="1530217204567" LINK="https://goo.gl/w94xnx"/>
+</node>
+</node>
+<node TEXT="Felipe Gaucho Pereira" ID="ID_1989658659" CREATED="1530218356742" MODIFIED="1530218382290">
+<node TEXT="Blog: Medium" ID="ID_477129735" CREATED="1530218383941" MODIFIED="1530218399006">
+<node TEXT="Link: https://goo.gl/XAoeFS" ID="ID_1032500321" CREATED="1530218408888" MODIFIED="1530218825592" LINK="https://goo.gl/XAoeFS"/>
+</node>
+</node>
 </node>
 <node TEXT="Fora" FOLDED="true" POSITION="right" ID="ID_1296874812" CREATED="1530210247602" MODIFIED="1530210250248">
 <edge COLOR="#ff00ff"/>
@@ -158,7 +217,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -167,6 +225,12 @@
 </node>
 <node TEXT="Podcasts" POSITION="right" ID="ID_1946179893" CREATED="1530212714967" MODIFIED="1530212718637">
 <edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Whitepapers" POSITION="right" ID="ID_1312631644" CREATED="1530218718835" MODIFIED="1530218729584">
+<edge COLOR="#00007c"/>
+<node TEXT="Trubit: A Scalable Verification Solution for Blockchains" ID="ID_1912422421" CREATED="1530218730921" MODIFIED="1530218783869">
+<node TEXT="Link: https://goo.gl/1kDNmC" ID="ID_1588667402" CREATED="1530218804639" MODIFIED="1530218818695" LINK="https://goo.gl/1kDNmC"/>
+</node>
 </node>
 </node>
 </map>
