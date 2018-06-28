025947f89f55627f32cd200c7fcaa2042f3c2b6a
Peter Alexander <peter.alexander@prodatalab.com>
2018-06-28 13:07:19 -0600

    modified todo-for-launch.mm ..added token-engineering.mm

---
+++ docs/mindmaps/token-engineering.mm
@@ -0,0 +1,172 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Token Engineering" FOLDED="false" ID="ID_1047944362" CREATED="1530209264328" MODIFIED="1530209272987" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Glossary" FOLDED="true" POSITION="right" ID="ID_1537660174" CREATED="1530209344523" MODIFIED="1530209350163">
+<edge COLOR="#ff0000"/>
+<node TEXT="Token Engineering" ID="ID_633833220" CREATED="1530209351935" MODIFIED="1530209357200"/>
+<node TEXT="Crypto-Asset Valuation" ID="ID_140430192" CREATED="1530209358344" MODIFIED="1530209366845"/>
+<node TEXT="Token Curated Design" ID="ID_4016597" CREATED="1530209367879" MODIFIED="1530209385311"/>
+<node TEXT="Token Registry" ID="ID_881453941" CREATED="1530209385937" MODIFIED="1530209392673"/>
+<node TEXT="Game Theory" ID="ID_242635353" CREATED="1530209397045" MODIFIED="1530209401327"/>
+<node TEXT="Mechanism Design" ID="ID_913081684" CREATED="1530209401897" MODIFIED="1530209410295"/>
+</node>
+<node TEXT="Articles" POSITION="right" ID="ID_522131657" CREATED="1530209484869" MODIFIED="1530209570978">
+<edge COLOR="#0000ff"/>
+<node TEXT="Can Blockchains Go Rogue?" FOLDED="true" ID="ID_661301769" CREATED="1530211415445" MODIFIED="1530211423680">
+<node TEXT="Author: Trent McConaghy" ID="ID_433808824" CREATED="1530211608424" MODIFIED="1530211623843"/>
+<node TEXT="Link: https://goo.gl/GGRYWb" ID="ID_804736429" CREATED="1530211626430" MODIFIED="1530211738315" LINK="https://goo.gl/dNBJHk"/>
+<node TEXT="Note: TE Series Part I" ID="ID_485802248" CREATED="1530211639291" MODIFIED="1530211992221"/>
+</node>
+<node TEXT="Towards a Practice of Token Engineering" FOLDED="true" ID="ID_1829440764" CREATED="1530209585687" MODIFIED="1530211772591">
+<node ID="ID_972820075" CREATED="1530211481715" MODIFIED="1530211481715"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Author: Trent McConaghy
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1920611877" CREATED="1530211481715" MODIFIED="1530211481715" LINK="https://goo.gl/dNBJHk"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Link: https://goo.gl/dNBJHk
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Note: TE Series Part II" ID="ID_298713392" CREATED="1530211481716" MODIFIED="1530211967176"/>
+</node>
+<node TEXT="Token Engineering Case Studies" FOLDED="true" ID="ID_1090562654" CREATED="1530211858302" MODIFIED="1530211874261">
+<node TEXT="" ID="ID_1173796725" CREATED="1530212036645" MODIFIED="1530212036645"/>
+<node TEXT="Author: Trent McConaghy" ID="ID_1618280646" CREATED="1530211878048" MODIFIED="1530211890982"/>
+<node TEXT="Link: https://goo.gl/jrwGP4" ID="ID_1003672722" CREATED="1530211893104" MODIFIED="1530211974701" LINK="https://goo.gl/jrwGP4"/>
+<node TEXT="Note: TE Series Part III" ID="ID_1892717896" CREATED="1530211929301" MODIFIED="1530211944821"/>
+</node>
+<node TEXT="Curated Proofs Markets: A Walk-Through of Ocean&apos;s Core Token Mechanics" FOLDED="true" ID="ID_273255117" CREATED="1530212168910" MODIFIED="1530212197001">
+<node TEXT="Author: Trent McConoghy" ID="ID_629207204" CREATED="1530212208868" MODIFIED="1530212218968"/>
+<node TEXT="Link: https://goo.gl/vRYRha" ID="ID_1065379716" CREATED="1530212220601" MODIFIED="1530212249210" LINK="https://goo.gl/vRYRha"/>
+<node TEXT="Note: See appendix for link to video and slides" ID="ID_1879803182" CREATED="1530212253545" MODIFIED="1530212294406"/>
+</node>
+<node TEXT="The Layered TCR" ID="ID_436277814" CREATED="1530212344350" MODIFIED="1530212353550">
+<node TEXT="Author: Trent McConaghy" ID="ID_1261976898" CREATED="1530212357218" MODIFIED="1530212385515"/>
+<node TEXT="Link: https://goo.gl/QRD2j7" ID="ID_1037574415" CREATED="1530212418462" MODIFIED="1530212438358" LINK="https://goo.gl/QRD2j7"/>
+<node TEXT="About: Introduction to LTCRs" ID="ID_732462128" CREATED="1530212442366" MODIFIED="1530212512677"/>
+</node>
+<node TEXT="Towards a Hierarchy of Token Building Blocks" ID="ID_1029684421" CREATED="1530212532921" MODIFIED="1530212559287">
+<node TEXT="Author: Trent McConaghy" ID="ID_411737533" CREATED="1530212580251" MODIFIED="1530212593859"/>
+<node TEXT="Link: https://goo.gl/YdrP5K" ID="ID_1398676005" CREATED="1530212594525" MODIFIED="1530212629451" LINK="https://goo.gl/YdrP5K"/>
+<node TEXT="About: Design Methodology" ID="ID_1031907052" CREATED="1530212631676" MODIFIED="1530212667175"/>
+</node>
+</node>
+<node TEXT="People" FOLDED="true" POSITION="right" ID="ID_904425458" CREATED="1530210206498" MODIFIED="1530210209612">
+<edge COLOR="#00ff00"/>
+<node TEXT="Trent McConaghy" ID="ID_743269120" CREATED="1530210210953" MODIFIED="1530210221432">
+<node TEXT="Blog: Medium" ID="ID_988492645" CREATED="1530211220162" MODIFIED="1530211233058">
+<node TEXT="Link: https://goo.gl/niigze" ID="ID_275209671" CREATED="1530211233063" MODIFIED="1530211281508" LINK="https://goo.gl/niigze"/>
+</node>
+</node>
+<node TEXT="Chris Burniske" ID="ID_1888160032" CREATED="1530210222096" MODIFIED="1530210228545"/>
+</node>
+<node TEXT="Fora" FOLDED="true" POSITION="right" ID="ID_1296874812" CREATED="1530210247602" MODIFIED="1530210250248">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Cryptoeconomics.hub" ID="ID_684326495" CREATED="1530210295534" MODIFIED="1530210395853"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Where: Telegram Chat App
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Videos" POSITION="right" ID="ID_901897082" CREATED="1530212710480" MODIFIED="1530212714121">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Podcasts" POSITION="right" ID="ID_1946179893" CREATED="1530212714967" MODIFIED="1530212718637">
+<edge COLOR="#7c0000"/>
+</node>
+</node>
+</map>
